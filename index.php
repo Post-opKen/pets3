04eeaa5b87f60311cdc5c6155949f8d143649439
Ean Daus <eandaus123@gmail.com>
2019-01-25 15:26:35 -0500

    added validation for the forms

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
+require_once('model/validation-functions.php');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
@@ -54,21 +55,45 @@ $f3->route('GET /@animal', function($f3, $params) {
 });
 
 //Define a form1 route
-$f3->route('GET|POST /order', function() {
-    $view = new View();
-    echo $view->render('views/form1.html');
+$f3->route('GET|POST /order', function($f3) {
+    $_SESSION = array();
+
+    if(isset($_POST['animal']))
+    {
+        $animal = $_POST['animal'];
+        if(validString($animal))
+        {
+            $_SESSION['animal'] = $animal;
+            $f3->reroute('/order2');
+        }else{
+            $f3->set("errors['animal']", "Please enter an animal");
+            echo $f3->errors['animal'];
+        }
+    }
+    $template = new Template();
+    echo $template->render('views/form1.html');
 });
 
 //Define a form2 route
-$f3->route('GET|POST /order2', function() {
-    $_SESSION['animal'] = $_POST['animal'];
+$f3->route('GET|POST /order2', function($f3) {
+    if(isset($_POST['color']))
+    {
+        $color = $_POST['color'];
+        if(validColor($color))
+        {
+            $_SESSION['color'] = $color;
+            $f3->reroute('/results');
+        }else{
+            $f3->set("errors['color']", "Please choose a valid color");
+            echo $f3->errors['color'];
+        }
+    }
     $template = new Template();
     echo $template->render('views/form2.html');
 });
 
 //Define a results route
-$f3->route('POST /results', function() {
-    $_SESSION['color'] = $_POST['color'];
+$f3->route('POST|GET /results', function() {
     $template = new Template();
     echo $template->render('views/results.html');
 });
