06d9794efa51976334c6c9c4217795af7437e467
Brandon Skar <bransk01@yahoo.com>
2019-01-25 13:39:56 -0500

    Added colors to drop down

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $f3 = Base::instance();
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+$f3->set('colors', array('pink', 'green', 'blue'));
 //Define a default route
 $f3->route('GET /', function() {
     echo"<h1>My Pets</h1>";
@@ -61,8 +62,8 @@ $f3->route('GET /order', function() {
 //Define a form2 route
 $f3->route('POST /order2', function() {
     $_SESSION['animal'] = $_POST['animal'];
-    $view = new View();
-    echo $view->render('views/form2.html');
+    $template = new Template();
+    echo $template->render('views/form2.html');
 });
 
 //Define a results route
